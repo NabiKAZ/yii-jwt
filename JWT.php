369bdc1e8a44fcda09de56fe2cf5cf4799cd8e9d
Nabi <nabikaz@gmail.com>
2017-03-13 05:17:28 +0330

    fixed #1, Include Exceptions
    Signed-off-by: Nabi <nabikaz@gmail.com>

--- JWT.php
+++ JWT.php
@@ -19,6 +19,10 @@ class JWT
     public function init()
     {
         require dirname(__FILE__) . '/src/JWT.php';
+        require dirname(__FILE__) . '/src/BeforeValidException.php';
+        require dirname(__FILE__) . '/src/ExpiredException.php';
+        require dirname(__FILE__) . '/src/SignatureInvalidException.php';
+
     }
 
     public function encode($payload)
